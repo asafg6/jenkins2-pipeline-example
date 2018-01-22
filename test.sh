a7def45ba4911ae0db1d2e07ce69a356f266b492
asaf <asaf.gu6@gmail.com>
2018-01-22 19:15:18 +0200

    added test env var

--- test.sh
+++ test.sh
@@ -5,5 +5,11 @@ if [ $CONTENTS_OF_MY_AMAZING_FILE != "built!" ]; then
 	echo "test failed!"
 	exit 1
 fi
+
+if [ $MY_AWESOME_ENV_VAR != "should_be_true" ]; then
+	echo "no env var... oh no :("
+	exit 1
+fi
+
 echo "test successful"
 exit 0
